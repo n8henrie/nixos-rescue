1108e5abb710b07b1576b7004433b5a4f1427cfc
Nathan Henrie <nate@n8henrie.com>
2023-01-14 21:36:33 -0700

    Update defaults

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,6 @@
 , options
 , specialArgs
 }: {
-
   imports = [
     "${modulesPath}/profiles/all-hardware.nix"
     "${modulesPath}/profiles/base.nix"
@@ -14,33 +13,70 @@
 
   isoImage = {
     compressImage = true;
-    makeEfiBootable = true;
-    makeUsbBootable = true;
+    isoName = lib.mkForce "rescue.iso";
+    appendToMenuLabel = " Rescue";
   };
 
   boot = {
     binfmt.emulatedSystems = [ "aarch64-linux" ];
     kernelParams = [ "copytoram" ];
     kernelModules = [ ];
+    loader.timeout = lib.mkForce 0;
     supportedFilesystems = [
+      "apfs"
       "btrfs"
-      "squashfs"
+      "exfat"
+      "ext2"
+      "ext4"
+      "ntfs"
       "vfat"
+      "xfs"
       "zfs"
     ];
-    loader.grub.memtest86.enable = true;
   };
+
+  systemd.services = {
+    wpa_supplicant.wantedBy = lib.mkForce [ "multi-user.target" ];
+    sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
+  };
+  services.nscd.enableNsncd = true;
+
+  networking = {
+    hostName = "rescue";
+    domain = "home.arpa";
+    useDHCP = true;
+  };
+
   environment = {
+    # Many of these are unecessary (automatically imported due to
+    # supportedFilesystems, but this makes it explicit)
     systemPackages = with pkgs; [
       arch-install-scripts
       bashInteractive
+      ddrescue
+      efibootmgr
+      efivar
       fd
+      gawk
       git
+      gptfdisk
+      hdparm
       jq
+      mkpasswd
       neovim
       parted
+      pciutils
+      python3
       ripgrep
       rsync
+      screen
+      sdparm
+      smartmontools
+      tmux
+      unzip
+      usbutils
+      zip
+      zstd
     ];
     variables = {
       EDITOR = "nvim";
@@ -50,23 +86,23 @@
     ];
   };
 
-  users.defaultUserShell = pkgs.bashInteractive;
-
-  # https://github.com/NixOS/nixpkgs/blob/a6968ad43c1b6ab7fc341ddba4ed0a082a24229b/nixos/modules/profiles/installation-device.nix#L36
-  # services = {
-  #   mingetty.autologinUser = "root";
-  #   openssh = {
-  #     enable = true;
-  #     permitRootLogin = "yes";
-  #   };
-  # };
-
-  # networking.wireless.enable = mkDefault true;
-  # systemd.services.wpa_supplicant.wantedBy = mkOverride 50 [ ];
-
-  nixpkgs.config = {
-    allowUnfree = true;
+  users = {
+    defaultUserShell = pkgs.bashInteractive;
+    mutableUsers = false;
+    users = {
+      root = {
+        password = "nixos-rescue";
+        openssh.authorizedKeys.keyFiles = [
+          (builtins.fetchurl {
+            url = "https://github.com/n8henrie.keys";
+            sha256 = "1zhq1r83v6sbrlv1zh44ja70kwqjifkqyj1c258lki2dixqfnjk7";
+          })
+        ];
+      };
+    };
   };
+
+  nixpkgs.config.allowUnfree = true;
 
   nix = {
     settings = {
