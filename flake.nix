82976b2e158a859982c9e6f89c6772d8f271551a
Nathan Henrie <nate@n8henrie.com>
2023-01-14 21:36:58 -0700

    Update inputs

--- flake.nix
+++ flake.nix
@@ -1,15 +1,27 @@
 {
   description = "NixOS-based rescue drive";
-  inputs.nixos.url = "nixpkgs/nixos-unstable";
-  outputs = { self, nixos }: {
-    packages.x86_64-linux = rec {
-      default = rescue.config.system.build.isoImage;
-      rescue = nixos.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      nixos =
+        let src = (import nixpkgs { inherit system; }).applyPatches {
+          name = "nixos-rescue-patches";
+          src = nixpkgs;
+          patches = [ ./iso-image.patch ];
+        };
+        in
+        nixpkgs.lib.fix (self: (import "${src}/flake.nix").outputs { inherit self; });
+    in
+    {
+      packages.x86_64-linux = rec {
+        default = rescue.config.system.build.isoImage;
+        rescue = nixos.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+          ];
+        };
       };
     };
-  };
 }
